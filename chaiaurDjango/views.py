a72cd387c2bc50611c6a70371085ffb7685c8601
iamdeepakdass <iamdeepakdass@gmail.com>
2024-09-11 12:38:17 +0530

    Add render to the contact and about page

--- chaiaurDjango/views.py
+++ chaiaurDjango/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 
  
 def home(request):
-    #return HttpResponse("Hello, World! You are at the chaiaurDjango home page.")
-    return render(request, 'index.html')
+    # return HttpResponse("Hello, World! You are at the chaiaurDjango home page.")
+    return render(request, 'website/index.html')
 
 def about(request):
-    return HttpResponse("Hello, World! You are at the chaiaurDjango about page.")
+    # return HttpResponse("Hello, World! You are at the chaiaurDjango about page.")
+    return render(request, 'website/about.html')
     
 def contact(request):
-    return HttpResponse("Hello, World! You are at the chaiaurDjango contact page.")
+    # return HttpResponse("Hello, World! You are at the chaiaurDjango contact page.")
+    return render(request, 'website/contact.html')
 
 # def home(request):
 #     return HttpResponse("Hello, World! You are at the chaiaurDjango home page.")
